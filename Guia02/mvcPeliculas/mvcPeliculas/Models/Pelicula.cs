512aef14a11696c02568eec9c7afca526d10dc93
Napote <palenciabnathaly@gmail.com>
2021-09-12 00:06:05 -0600

    Actualizando framework a 5.0 en guia2

---
+++ Guia02/mvcPeliculas/mvcPeliculas/Models/Pelicula.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvcPeliculas.Models
+{
+    public class Pelicula
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        [Display(Name = "Fecha de Lanzamiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FechaLanzamiento { get; set; }
+        public string Genero { get; set; }
+
+        [Column(TypeName = "decimal(18, 0)")]
+        public decimal Precio { get; set; }
+    }
+
+    public class PeliculaDBContext : DbContext
+    {
+        public DbSet<Pelicula> Peliculas { get; set; }
+
+        public PeliculaDBContext(DbContextOptions options)
+          : base(options)
+        {
+
+        }
+
+    }
+}
